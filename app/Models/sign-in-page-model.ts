ba214b9f1122cf0cb2187df51aefed02ee7d158b
Skysoul <rafael@skysoul.com.au>
2016-02-23 13:04:48 +0800

    Fix Stashed Git Conflict File

--- app/Models/sign-in-page-model.ts
+++ app/Models/sign-in-page-model.ts
@@ -5,63 +5,14 @@ import {cache} from "../Modules/Helpers";
 
 export class SignInPageModel extends Observable {
 
+    /**
+     * Sing in Page
+     */
     public tapSignIn() {
 
         /**
          * Set Loader Spinner
          */
-<<<<<<< HEAD
-        var _this = this;
-        _this.set('isLoading', true);
-        var onSuccess = function() {
-            App.navigate.to('main-page');
-            _this.set('isLoading', false);
-            },
-            onError = function(errors) {
-                App.api.alertErrors(errors);
-                _this.set('isLoading', false);
-            },
-            data = {
-                credential: _this.get('username'),
-                password: _this.get('password')
-            };
-        App.api.fetch('login', data, onSuccess, onError);
-        // http.request({
-        //     url: App.api.get('login').url,
-        //     method: "POST",
-        //     headers: {"Content-Type": "application/json"},
-        //     content: JSON.stringify({
-        //         credential: this.get('username'),
-        //         password: this.get('password')
-        //     })
-        // }).then(function (response) {
-
-        //     /**
-        //      * Disable Loader
-        //      */
-        //     _this.set('isLoading', false);
-
-        //     var result = response.content.toJSON();
-
-        //     /**
-        //      * Store Token
-        //      */
-        //     if (result.hasOwnProperty('api_token')) {
-        //         cache.set('user', result);
-        //         App.navigate.to('main-page');
-        //     }
-
-        //     /**
-        //      * Alert Errors
-        //      */
-        //     if (result.hasOwnProperty('error')) {
-        //         App.api.alertErrors(result.error);
-        //     }
-
-        // }, function (e) {
-        //     console.log("Error occurred " + e);
-        // });
-=======
         this.set('isLoading', true);
 
         var _this = this, data = {
@@ -80,7 +31,6 @@ export class SignInPageModel extends Observable {
             };
 
         App.api.fetch('login', data, onSuccess, onError);
->>>>>>> 9ba69f1764a7e5489620bf99c297b7c50c8993c6
 
     }
 
