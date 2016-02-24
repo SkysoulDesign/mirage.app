ed2aae22fb86dd053b441e06f62a2ac4a72e87e0
Milewski <rafael.milewski@gmail.com>
2016-02-24 23:36:25 +0800

    FIxed Case

--- app/Views/video/video.android.ts
+++ app/Views/video/video.android.ts
@@ -1,31 +1,38 @@
-var mVideoView;
-export function createVideoView(args) {
-    console.log("createVideoView");
-    //create videoview    
-    mVideoView = new android.widget.VideoView(args.context);
-    var mMediaController = new android.widget.MediaController(args.context);
-    mMediaController.setAnchorView(mVideoView);
+import {CreateViewEventData} from "ui/placeholder";
+
+export function createVideoView(placeholder:CreateViewEventData) {
+
+
+    var videoView = new android.widget.VideoView(placeholder.context),
+        mMediaController = new android.widget.MediaController(placeholder.context);
+        mMediaController.setAnchorView(videoView);
+
     var params = new android.view.ViewGroup.LayoutParams(android.view.ViewGroup.LayoutParams.MATCH_PARENT, 1);
-    mVideoView.setLayoutParams(params);   
+
+
     // parse the uri
     var videoLink = 'http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4';
     // var videoLink = 'rtsp://192.168.1.253:554/bunny.mp4';
     var mVideoURL = android.net.Uri.parse(videoLink);
-    mVideoView.setVideoURI(mVideoURL);
-    mVideoView.setMediaController(mMediaController);
-    mVideoView.requestFocus();
-    mVideoView.start();
 
-    args.view = mVideoView;
+    videoView.setLayoutParams(params);
+    videoView.setVideoURI(mVideoURL);
+    videoView.setMediaController(mMediaController);
+    videoView.requestFocus();
+    videoView.start();
+
+    placeholder.view = videoView;
+
     switchfullscreen(false);
+
     // Create our Complete Listener - this is triggered once a video reaches the end
     var completionListener = new android.media.MediaPlayer.OnCompletionListener({
-        onCompletion: function(args) {
+        onCompletion: function (placeholder) {
             console.log('Video Done');
         }
     });
     // Set the listener using the correct method
-    mVideoView.setOnCompletionListener(completionListener);
+    videoView.setOnCompletionListener(completionListener);
 }
 
 function switchfullscreen(fullscreen) {
@@ -37,12 +44,12 @@ function switchfullscreen(fullscreen) {
         layoutParams.addRule(RelativeLayout.ALIGN_PARENT_TOP);
         layoutParams.addRule(RelativeLayout.ALIGN_PARENT_LEFT);
         layoutParams.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
-        mVideoView.setLayoutParams(layoutParams);
+        videoView.setLayoutParams(layoutParams);
         fullscreen = true;//改变全屏/窗口的标记
     } else {//设置RelativeLayout的窗口模式
         var lp = new LayoutParams(320, 240);
         lp.addRule(RelativeLayout.CENTER_IN_PARENT);
-        mVideoView.setLayoutParams(lp);
+        videoView.setLayoutParams(lp);
         fullscreen = false;//改变全屏/窗口的标记
     }
 }
