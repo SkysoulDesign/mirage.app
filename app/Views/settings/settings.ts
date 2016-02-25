e3c86757afbb25c63d416982b6e478a04a5cc65e
Skysoul <rafael@skysoul.com.au>
2016-02-25 15:40:50 +0800

    Settings

--- app/Views/settings/settings.ts
+++ app/Views/settings/settings.ts
@@ -1,9 +1,12 @@
 import vmModule = require("../../Models/settings-page-model");
 import {Page} from 'ui/page';
+import {DropDown} from "nativescript-drop-down";
 
 export function pageLoaded(args) {
 
-    var page = <Page>args.object;
-        page.bindingContext = new vmModule.SettingsPageModel();
+    var page = <Page>args.object,
+        dropdown = <DropDown>page.getViewById('dropdown');
+
+    page.bindingContext = new vmModule.SettingsPageModel(dropdown);
 
 }
