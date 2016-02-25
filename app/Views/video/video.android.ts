512e8c5185565d11293f7d2d0c675f43e09afcf2
Skysoul <rafael@skysoul.com.au>
2016-02-25 09:22:31 +0800

    Fixed undefined videoView

--- app/Views/video/video.android.ts
+++ app/Views/video/video.android.ts
@@ -2,10 +2,9 @@ import {CreateViewEventData} from "ui/placeholder";
 
 export function createVideoView(placeholder:CreateViewEventData) {
 
-
     var videoView = new android.widget.VideoView(placeholder.context),
         mMediaController = new android.widget.MediaController(placeholder.context);
-        mMediaController.setAnchorView(videoView);
+    mMediaController.setAnchorView(videoView);
 
     var params = new android.view.ViewGroup.LayoutParams(android.view.ViewGroup.LayoutParams.MATCH_PARENT, 1);
 
@@ -23,6 +22,25 @@ export function createVideoView(placeholder:CreateViewEventData) {
 
     placeholder.view = videoView;
 
+    var switchfullscreen = function (fullscreen) {
+        var LayoutParams = android.widget.RelativeLayout.LayoutParams;
+        var RelativeLayout = android.widget.RelativeLayout;
+        if (!fullscreen) {//设置RelativeLayout的全屏模式
+            var layoutParams = new LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.FILL_PARENT);
+            layoutParams.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
+            layoutParams.addRule(RelativeLayout.ALIGN_PARENT_TOP);
+            layoutParams.addRule(RelativeLayout.ALIGN_PARENT_LEFT);
+            layoutParams.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
+            videoView.setLayoutParams(layoutParams);
+            fullscreen = true;//改变全屏/窗口的标记
+        } else {//设置RelativeLayout的窗口模式
+            var lp = new LayoutParams(320, 240);
+            lp.addRule(RelativeLayout.CENTER_IN_PARENT);
+            videoView.setLayoutParams(lp);
+            fullscreen = false;//改变全屏/窗口的标记
+        }
+    };
+
     switchfullscreen(false);
 
     // Create our Complete Listener - this is triggered once a video reaches the end
@@ -35,21 +53,3 @@ export function createVideoView(placeholder:CreateViewEventData) {
     videoView.setOnCompletionListener(completionListener);
 }
 
-function switchfullscreen(fullscreen) {
-    var LayoutParams = android.widget.RelativeLayout.LayoutParams;
-    var RelativeLayout = android.widget.RelativeLayout;
-    if (!fullscreen) {//设置RelativeLayout的全屏模式
-        var layoutParams = new LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.FILL_PARENT);
-        layoutParams.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
-        layoutParams.addRule(RelativeLayout.ALIGN_PARENT_TOP);
-        layoutParams.addRule(RelativeLayout.ALIGN_PARENT_LEFT);
-        layoutParams.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
-        videoView.setLayoutParams(layoutParams);
-        fullscreen = true;//改变全屏/窗口的标记
-    } else {//设置RelativeLayout的窗口模式
-        var lp = new LayoutParams(320, 240);
-        lp.addRule(RelativeLayout.CENTER_IN_PARENT);
-        videoView.setLayoutParams(lp);
-        fullscreen = false;//改变全屏/窗口的标记
-    }
-}
