1a168ee62eae9fb478eee49576d331f5a86700c5
Skysoul <rafael@skysoul.com.au>
2016-02-26 18:54:50 +0800

    Register Page

--- app/Views/register-product/register-product.ts
+++ app/Views/register-product/register-product.ts
@@ -5,8 +5,9 @@ import vmModule = require("../../Models/register-product-page-model");
 export function pageLoaded(args){
 
 	var page = <Page>args.object;
-	var str = page.navigationContext;
+	var input = page.getViewById('code_input');
+	var button = page.getViewById('button_register');
 
-	page.bindingContext = new vmModule.RegisterProductPageModel(str);
+	page.bindingContext = new vmModule.RegisterProductPageModel(input,button);
 
 }
