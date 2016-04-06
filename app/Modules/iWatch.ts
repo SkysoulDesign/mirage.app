7b96191abacd7d55aa71d6d79466bf1a30f71097
Skysoul <rafael@skysoul.com.au>
2016-04-06 19:10:37 +0800

    Fixed Video Api

--- app/Modules/iWatch.ts
+++ app/Modules/iWatch.ts
@@ -1,14 +1,60 @@
 import application = require("application");
+import {database, navigate} from "./Helpers";
 
 export class iWatch {
 
     public session;
 
     public delegate = NSObject.extend({
+
+        session: null,
+
+        /**
+         * Send Message to iWatch
+         */
+        sendMessage: (message:{})=> {
+            console.log("sending message")
+            this.session.sendMessageReplyHandlerErrorHandler(message, null, null);
+        },
+
+        /**
+         * Send cache to iWatch
+         */
+        sendCache: ()=> {
+            console.log("sending cache")
+            this.sendMessage({products: database.query('login')});
+        },
+
+        /**
+         * Plays video with specific ID on IOS
+         * @param extraID
+         */
+        playIOSVideo: (extraID:Number) => {
+            console.log("play video" + extraID)
+            navigate.to('video', {context: {id: extraID}})
+        },
+
         sessionDidReceiveMessage: function (session, data) {
-            // var message = data.descriptionInStringsFileFormat.concat("oi");
-            //console.dir(data.valueForKey('message'));
-            // console.dir(JSON.parse("{"+message+"}"));
+
+            console.dir(data.valueForKey('paired'));
+
+            /**
+             * Set Session
+             */
+            this.session = session;
+
+            /**
+             * iWatch just paired, it needs some Cache
+             */
+            if (data.valueForKey('paired'))
+                this.sendCache();
+
+            /**
+             * User Played Video
+             */
+            if (data.valueForKey('play_video'))
+                this.playIOSVideo(data.valueForKey('play_video'));
+
         }
     }, {
         name: "delegate",
