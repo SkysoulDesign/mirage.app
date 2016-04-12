0040e432a7e4d6903b9b5b0efc4c8406d4a7f42b
Skysoul <rafael@skysoul.com.au>
2016-04-12 10:48:59 +0800

    iWatch

--- platforms/ios/iWatch Mirage 3D Extension/InterfaceController.swift
+++ platforms/ios/iWatch Mirage 3D Extension/InterfaceController.swift
@@ -8,35 +8,103 @@
 
 import WatchKit
 import Foundation
+import WatchConnectivity
 
 
-class InterfaceController: WKInterfaceController {
+class InterfaceController: WKInterfaceController, WCSessionDelegate {
     
     @IBOutlet var table: WKInterfaceTable!
     
-    var products = ["Batman", "Super Man", "Wonder Woman"];
+    var products = [[String:AnyObject]]()
+    
+    var session:WCSession!
+    var connected:Bool = false;
+    
+    func session(session: WCSession, didReceiveMessage message: [String : AnyObject]) {
+        
+        let values = message.values;
+        
+        if !values.isEmpty {
+
+            /* Connect Device Device */
+            if((message["paired"]) != nil){
+                connected = message["paired"] as! Bool;
+                print("device paired");
+            }
+            
+            /* Error Message*/
+            if((message["error"]) != nil){             
+                print(message["error"]);
+            }
+            
+            /* Received Products */
+            if((message["products"]) != nil){
+                
+                print("received products");
+                
+                let items = message["products"];
+                
+                table.setNumberOfRows(items!.count, withRowType: "ProductRow");
+                
+                for index in 0..<items!.count{
+                    
+                    let product = message["products"]![index]["product"]
+                    let row = table.rowControllerAtIndex(index) as! ProductRow
+                        row.productName.setText(product!!["name"] as? String);
+                    
+                    let collection = [
+                        "name": (product!!["name"] as? String)!,
+                        "profile": (product!!["profile"]!!["description"] as? String)!,
+                        "extraID": (product!!["extras"]!![0]["id"] as? Int)!
+                    ]
+                    
+                    /** Append to clobal */
+                    products.append(collection as! [String : AnyObject]);
+                    
+                    
+                }
+          
+
+            }
+            
+        }
+        
+        
+    }
     
     override func awakeWithContext(context: AnyObject?) {
         super.awakeWithContext(context)
         
-        let userDefaults = NSUserDefaults(suiteName: "group.com.soap.tumbler.Mirage3D");
-        userDefaults!.synchronize();
+        print("hi");
         
-        print(userDefaults!.objectForKey("shared"));
-        
-        if let restoredValue = userDefaults!.objectForKey("shared") {
-            products = restoredValue as! [String]
-        }
-        else {
-            products = ["nothing", "there", "god"];
+        if(WCSession.isSupported()){
+            session = WCSession.defaultSession()
+            session.delegate = self
+            session.activateSession()
         }
         
-        table.setNumberOfRows(products.count, withRowType: "ProductRow");
+        /** get cache **/
+        session.sendMessage(["paired": true], replyHandler: nil, errorHandler: nil)
+
         
-        for index in 0..<products.count{
-            let row = table.rowControllerAtIndex(index) as! ProductRow
-            row.productName.setText(products[index]);
-        }
+//        let userDefaults = NSUserDefaults(suiteName: "group.com.soap.tumbler.Mirage3D");
+//        userDefaults!.synchronize();
+//        
+//        print(userDefaults!.objectForKey("shared"));
+//        
+//        if let restoredValue = userDefaults!.objectForKey("shared") {
+//            products = restoredValue as! [String]
+//        }
+//        else {
+//            products = ["nothing", "there", "god"];
+//        }
+        
+//        table.setNumberOfRows(products.count, withRowType: "ProductRow");
+//        
+//        for index in 0..<products.count{
+//            let row = table.rowControllerAtIndex(index) as! ProductRow
+//            row.productName.setText(products[index]);
+//        }
         
     }
     
